bd6eceb8ed90bffe1a0317fc5eb3c68a311c0c69
Rodrigo Real <rodrigoreal@live.com>
2017-02-15 22:13:45 -0200

    feat(pages/page1): get reddit feed and show on a list

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+
+import { Reddit } from '../../providers/reddit';
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
-
-  constructor(public navCtrl: NavController) {
-    
+  feed: any;
+  constructor(public navCtrl: NavController, private reddit: Reddit) {
+    reddit.get().subscribe((response) => {
+      this.feed = response;
+    })
   }
-
 }
